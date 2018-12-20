8bb860fc8f484527bc0a92c7eff4e1d2add37a15
Illya Sokolov <illya.sokolov82@gmail.com>
2018-12-20 16:02:19 +0200

    Adding task11 on github

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1101/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task11.task1101;
+
+/* 
+Лошадь и Пегас
+Отредактировать два класса: Horse (лошадь) и Pegasus (пегас).
+Унаследовать пегаса от лошади.
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse {
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task11/task1121/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task11.task1121;
+
+/* 
+Чужой код, незнакомый
+Исправь наследование в классах: (классы Cat, Dog, Pet, House, Airplane).
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Pet /*extends House*/ {
+
+    }
+
+    public class Cat extends Pet /*extends Airplane*/ {
+
+    }
+
+    public class Dog extends Pet/*extends Cat*/ {
+
+    }
+
+    public class House /*extends Dog*/ {
+
+    }
+
+    public class Airplane {
+
+    }
+}
