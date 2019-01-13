806fcffd89f015067a8919fbede827b15c9a487d
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-13 19:20:31 +0200

    Racer(2)

--- 8.Games/src/com/javarush/games/racer/RacerGame.java
+++ 8.Games/src/com/javarush/games/racer/RacerGame.java
@@ -7,10 +7,22 @@ public class RacerGame extends Game {
 
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    public static final int CENTER_X = WIDTH / 2;
+    public static final int ROADSIDE_WIDTH = 14;
 
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
         showGrid(false);
+        createGame();
+    }
+
+    private void drawField() {}
+
+    private void drawScene() {
+        drawField();
+    }
+    private void createGame() {
+        drawScene();
     }
 }
