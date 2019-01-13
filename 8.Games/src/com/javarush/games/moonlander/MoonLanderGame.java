cb0eddcc5d558286e5df71034dc873be5076581f
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-13 18:34:54 +0200

    Moonlander(2)

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -10,5 +10,18 @@ public class MoonLanderGame extends Game {
     @Override
     public void initialize() {
         setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+    private void drawScene() {
+        for (int i = 0; i < WIDTH ; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                setCellColor(i, j, Color.BLUE);
+            }
+        }
+    }
+
+    private void createGame() {
+        drawScene();
     }
 }
