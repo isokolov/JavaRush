7db3f96f546b06a71abea777c69ce4ea2de67a64
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-16 21:33:07 +0200

    Arkanoid(10)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -35,4 +35,24 @@ public class Canvas {
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        int dx = (int) Math.round(x);
+        int dy = (int) Math.round(y);
+        if (x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length) {
+            return;
+        }
+         matrix[dy][dx] = c;
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
 }
