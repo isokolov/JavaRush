8b53ef3a3ea94a506089f094b57f5ea8b8e321fc
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-16 22:33:10 +0200

    Arkanoid(14)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -35,13 +35,23 @@ public class Ball extends BaseObject {
         return dy;
     }
 
+    public void start() {
+        isFrozen = false;
+    }
+
     @Override
     public void draw(Canvas canvas) {
-
+        canvas.setPoint(x, y, 'O');
     }
 
     @Override
     public void move() {
-
+        if (isFrozen) {
+            return;
+        }
+        if (!isFrozen) {
+            x += dx;
+            y += dy;
+        }
     }
 }
