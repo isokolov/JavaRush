a2d5e5e22e7e3f828bd94b07bd50e7f9b986e2de
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-16 21:56:54 +0200

    Arkanoid(12)

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -6,6 +6,12 @@ public class Brick extends BaseObject {
         super(x, y, radius);
     }
 
+    public Brick(double x, double y) {
+        super(x, y, 3);
+    }
+
+
+
     @Override
     public void draw(Canvas canvas) {
 
