dfd144859696651468b7de37d3a70c2647b4c71b
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-20 15:57:01 +0200

    MVC(4)

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -2,31 +2,22 @@ package com.javarush.task.task36.task3608;
 
 import com.javarush.task.task36.task3608.controller.Controller;
 import com.javarush.task.task36.task3608.model.FakeModel;
+import com.javarush.task.task36.task3608.model.MainModel;
 import com.javarush.task.task36.task3608.model.Model;
 import com.javarush.task.task36.task3608.view.UsersView;
 
 public class Solution {
     public static void main(String[] args) {
-        Model model = new FakeModel();
+        /*Model model = new FakeModel();*/
+        Model model = new MainModel();
         UsersView usersView = new UsersView();
         Controller controller = new Controller();
 
         usersView.setController(controller);
         controller.setModel(model);
-
+        
         controller.setUsersView(usersView);
-        controller.onShowAllUsers();  /* is working */
+        controller.onShowAllUsers();
 
-        /*usersView.fireEventShowAllUsers(); */ /* fails */
-
-        /* Model model = new FakeModel();
-        UsersView usersView = new UsersView();
-        Controller controller = new Controller();
-
-        usersView.setController(controller);
-        controller.setModel(model);
-        controller.setUsersView(usersView);
-
-        usersView.fireEventShowAllUsers();*/
     }
 }
