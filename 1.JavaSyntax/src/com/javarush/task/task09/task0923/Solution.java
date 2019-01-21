d803a73f7a98cd7222d8ddbc6b9e49b4302422b8
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-21 14:43:45 +0200

    task0923

--- 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0923/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task09.task0923;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /* 
 Гласные и согласные
@@ -13,6 +14,27 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Character> vowelsSentence = new ArrayList<>();
+        ArrayList<Character> noVowelsSentence = new ArrayList<>();
+        String myText = reader.readLine();
+
+        for (char c: myText.toCharArray()) {
+            if (isVowel(c)) {
+                vowelsSentence.add(c);
+            } else if (!Character.isSpaceChar(c)){
+                noVowelsSentence.add(c);
+            }
+        }
+
+        for(Character c: vowelsSentence) {
+            System.out.print(c + " ");
+        }
+        System.out.println("");
+
+        for(Character c: noVowelsSentence) {
+            System.out.print(c + " ");
+        }
     }
 
     // метод проверяет, гласная ли буква
