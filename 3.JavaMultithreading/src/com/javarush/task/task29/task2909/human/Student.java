46fbd96172fa723e56a596f7b7e489987a7b534a
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-23 16:30:15 +0200

    Refactoring(7)

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -27,12 +27,16 @@ public class Student extends UniversityPerson {
         return course;
     }
 
-    public void incAverageGradeBy01() {
+    /*public void incAverageGradeBy01() {
         averageGrade += 0.1;
     }
 
     public void incAverageGradeBy02() {
         averageGrade += 0.2;
+    }*/
+
+    public void incAverageGrade(double delta) {
+        averageGrade += delta;
     }
 
     public void setCourse(int value) {
@@ -48,12 +52,12 @@ public class Student extends UniversityPerson {
         return "Студент";
     }
 
-    public void setBeginningOfSession(int day, int month, int year) {
-        beginningOfSession = new Date(year, month, day);
+    public void setBeginningOfSession(Date date) {
+        beginningOfSession = date;
     }
 
-    public void setEndOfSession(int day, int month, int year) {
-        endOfSession = new Date(year, month, day);
+    public void setEndOfSession(Date date) {
+        endOfSession = date;
     }
 
     public double getAverageGrade() {
