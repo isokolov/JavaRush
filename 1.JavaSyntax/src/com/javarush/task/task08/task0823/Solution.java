6d4390cb3583720f04ca6e28b95ba5ba5ba7f607
Illya Sokolov <illya.sokolov82@gmail.com>
2019-01-27 15:22:25 +0200

    task0823 RegExp

--- 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0823/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.task.task08.task0823;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* 
 Омовение Рамы
@@ -13,7 +16,19 @@ public class Solution {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
 
+        s = s.replaceAll("\\s+", " ");
+        //System.out.println(s);
+
         //напишите тут ваш код
-        // Will do later
+        String[] str = s.split(" ");
+        for (int i = 0; i < str.length ; i++) {
+            str[i] = Character.toUpperCase(str[i].charAt(0)) + str[i].substring(1);
+        }
+        String newWord = "";
+        for (int i = 0; i < str.length ; i++) {
+            newWord = newWord + str[i] + " " ;
+        }
+
+        System.out.println(newWord);
     }
 }
