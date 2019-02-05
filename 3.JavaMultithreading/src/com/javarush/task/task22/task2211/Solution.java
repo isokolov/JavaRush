f1ebcce83583b933d8d9ec0cae3526f469fcb691
Illya Sokolov <illya.sokolov82@gmail.com>
2019-02-05 20:55:44 +0200

    Tetris(13)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2211/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task22.task2211;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/* 
+Смена кодировки
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //FileInputStream fileInputStream = new FileInputStream(args[0], Character.W)
+    }
+}
