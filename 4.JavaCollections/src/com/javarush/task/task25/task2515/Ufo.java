1ceb57f2e78a97fa7f5b4b344a4aa6edb518d110
Illya Sokolov <illya.sokolov82@gmail.com>
2019-02-11 17:12:58 +0200

    Space(14)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -2,7 +2,41 @@ package com.javarush.task.task25.task2515;
 
 public class Ufo extends BaseObject {
 
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    //картинка корабля для отрисовки
+    private static int[][] matrix = {
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1; //-1..1
+        double dy = Math.random() * 2 - 1; //-1..1
+        x = x + dx;
+        y = y + dx;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, 1, Space.game.getHeight() / 2);
+        if (Math.random() <= 0.1) {
+            fire();
+        }
+    }
+
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
