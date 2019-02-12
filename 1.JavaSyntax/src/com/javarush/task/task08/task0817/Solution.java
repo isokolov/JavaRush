34a0dbf8e0767245b0ee7bb19278298543ff4195
Illya Sokolov <illya.sokolov82@gmail.com>
2019-02-12 16:40:34 +0200

    ConcurentHashMap

--- 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0817/Solution.java
@@ -2,6 +2,7 @@ package com.javarush.task.task08.task0817;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /* 
 Нам повторы не нужны
@@ -10,12 +11,38 @@ import java.util.Map;
 public class Solution {
     public static HashMap<String, String> createMap() {
         //напишите тут ваш код
-        // Will do later
-        return null;
+        HashMap<String, String> hm =new HashMap<>();
+
+        hm.put("Иванов", "Иван");
+        hm.put("Иванидзе", "Иван");
+        hm.put("Иваник", "Петр");
+        hm.put("Петров", "Петр");
+        hm.put("Сидоров", "Сидр");
+        hm.put("Прутков", "Кузьма");
+        hm.put("Толстой", "Алексей");
+        hm.put("Тонкий", "Лев");
+        hm.put("Пушкин", "Михаил");
+        hm.put("Кукушкин", "Сергей");
+
+        return hm;
     }
 
     public static void removeTheFirstNameDuplicates(Map<String, String> map) {
         //напишите тут ваш код
+        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>(map);
+
+        for(Map.Entry<String, String> entry: map.entrySet()) {
+            for(Map.Entry<String, String> entryConcurent: concurrentHashMap.entrySet()) {
+                if (entry.getValue().equals(entryConcurent.getValue()) &&
+                !entry.getKey().equals(entryConcurent.getKey())) {
+                    removeItemFromMapByValue(concurrentHashMap, entryConcurent.getValue());
+                }
+            }
+        }
+        map.clear();
+        for(Map.Entry<String, String> entryConcurent: concurrentHashMap.entrySet()) {
+            map.put(entryConcurent.getKey(), entryConcurent.getValue());
+        }
 
     }
 
@@ -25,9 +52,13 @@ public class Solution {
             if (pair.getValue().equals(value))
                 map.remove(pair.getKey());
         }
+
     }
 
     public static void main(String[] args) {
-
+        /*HashMap<String, String> hm = createMap();
+        //System.out.println(hm);
+        removeTheFirstNameDuplicates(hm);
+        System.out.println(hm);*/
     }
 }
