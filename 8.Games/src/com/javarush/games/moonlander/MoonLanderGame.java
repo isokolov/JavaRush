b5d9f9a937f0b3ed9ea12f5c7916ba7af9fdc1f2
Illya Sokolov <illya.sokolov82@gmail.com>
2019-02-18 18:12:30 +0200

    moonlander(9)

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -26,6 +26,38 @@ public class MoonLanderGame extends Game {
     }
 
     @Override
+    public void onKeyPress(Key key) {
+        switch (key) {
+            case UP:
+                isUpPressed = true;
+                break;
+            case LEFT:
+                isLeftPressed = true;
+                isRightPressed = false;
+                break;
+            case RIGHT:
+                isRightPressed = true;
+                isLeftPressed = false;
+                break;
+        }
+    }
+
+    @Override
+    public void onKeyReleased(Key key) {
+        switch (key) {
+            case UP:
+                isUpPressed = false;
+                break;
+            case LEFT:
+                isLeftPressed = false;
+                break;
+            case RIGHT:
+                isRightPressed = false;
+                break;
+        }
+    }
+
+    @Override
     public void setCellColor(int x, int y, Color color) {
         if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) return;
         super.setCellColor(x, y, color);
