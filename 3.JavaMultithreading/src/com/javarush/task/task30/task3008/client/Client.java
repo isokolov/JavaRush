d116d880786b793fb3230790092418678ff7c31c
Illya Sokolov <illya.sokolov82@gmail.com>
2019-02-28 15:13:09 +0200

    Server(13)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,8 @@
 package com.javarush.task.task30.task3008.client;
 
-import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.*;
+
+import java.io.IOException;
 
 public class Client {
 
@@ -8,4 +10,35 @@ public class Client {
     private volatile boolean clientConnected = false;
 
     public class SocketThread extends Thread {}
+
+    protected String getServerAddress() {
+        //String serverAddress = ConsoleHelper.readString();
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException exc) {
+
+            ConsoleHelper.writeMessage("No connection established");
+            clientConnected = false;
+        }
+    }
 }
