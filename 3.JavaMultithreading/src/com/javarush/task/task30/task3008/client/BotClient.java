6f606642a0ef6e7c00401dd31c2cef0693c3f344
Illya Sokolov <illya.sokolov82@gmail.com>
2019-03-04 22:31:36 +0200

    Server(19)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,11 @@
 package com.javarush.task.task30.task3008.client;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class BotClient extends Client {
 
     public static void main(String[] args) {
@@ -9,6 +15,52 @@ public class BotClient extends Client {
 
     public class BotSocketThread extends SocketThread {
 
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException  {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
+
+
+        @Override
+        protected void processIncomingMessage(String message) {
+            super.processIncomingMessage(message);
+            if (message.contains(": ")) {
+                String name = message.split(": ")[0];
+                String txt = message.split(": ")[1];
+                SimpleDateFormat simpleDateFormat = null;
+                switch (txt) {
+                    case "дата":
+                        simpleDateFormat = new SimpleDateFormat("d.MM.YYYY");
+                        break;
+                    case "день":
+                        simpleDateFormat = new SimpleDateFormat("d");
+                        break;
+                    case "месяц":
+                        simpleDateFormat = new SimpleDateFormat("MMMM");
+                        break;
+                    case "год":
+                        simpleDateFormat = new SimpleDateFormat("YYYY");
+                        break;
+                    case "время":
+                        simpleDateFormat = new SimpleDateFormat("H:mm:ss");
+                        break;
+                    case "час":
+                        simpleDateFormat = new SimpleDateFormat("H");
+                        break;
+                    case "минуты":
+                        simpleDateFormat = new SimpleDateFormat("m");
+                        break;
+                    case "секунды":
+                        simpleDateFormat = new SimpleDateFormat("s");
+                        break;
+                    default:
+                        return;
+                }
+                sendTextMessage(String.format("Информация для %s: %s", name,
+                        simpleDateFormat.format(Calendar.getInstance().getTime())));
+            }
+        }
     }
 
     @Override
@@ -26,4 +78,6 @@ public class BotClient extends Client {
         int random = (int)(Math.random() * 100);
         return "date_bot_" + random;
     }
+
+
 }
