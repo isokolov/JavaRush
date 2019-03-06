1975906d8c539e3ba170ba82c61e14421ac941b2
Illya Sokolov <illya.sokolov82@gmail.com>
2019-03-06 17:07:17 +0200

    Game2048(5)

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -14,7 +14,38 @@ public class Game2048 extends Game {
         drawScene();
     }
 
-    private void createGame() {}
+    private void createGame() {
+        createNewNumber();
+        createNewNumber();
+    }
+
+    private void createNewNumber() {
+        int x = 0;
+        int y = 0;
+        do {
+            x = getRandomNumber(SIDE);
+            y = getRandomNumber(SIDE);
+
+        } while (gameField[x][y] != 0);
+            if (getRandomNumber(10) < 9) {
+                gameField[x][y] = 2;
+            } else {
+                gameField[x][y] = 4;
+            }
+    }
+
+    /*private void createNewNumber() {
+        int x = getRandomNumber(SIDE);
+        int y = getRandomNumber(SIDE);
+        int random = getRandomNumber(10);
+        if (gameField[x][y] == 0) {
+            if (getRandomNumber(10) == 9) {
+                gameField[x][y] = 4;
+            } else {
+                gameField[x][y] = 2;
+            }
+        }
+    }*/
 
     private void drawScene() {
 
