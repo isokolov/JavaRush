e5c0dcf4fa7940f52852c49b9ecf9c44a0649645
Illya Sokolov <illya.sokolov82@gmail.com>
2019-03-07 16:34:18 +0200

    Factory(2)

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory {
+
+    public Human getPerson(int age) {
+        if (age > 0 && age <= 12)
+            return new KidBoy();
+        else if (age > 12 && age <= 19)
+            return new TeenBoy();
+        else if (age > 19)
+            return new Man();
+        else return null;
+    }
+}
