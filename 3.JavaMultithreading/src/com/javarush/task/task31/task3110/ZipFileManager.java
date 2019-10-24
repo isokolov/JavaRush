844cf932542c08b4fd91dda8ac62d67ca1daf20e
Illya Sokolov <illya.sokolov82@gmail.com>
2019-10-24 15:29:26 +0300

    Archiver(2)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
 
@@ -11,6 +15,16 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+        ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+        zipOutputStream.putNextEntry(zipEntry);
+        InputStream inputStream = Files.newInputStream(source);
+        byte[] buff = new byte[1000];
+        while (inputStream.available() != -1)
+        {
+            int count = inputStream.read(buff);
+            zipOutputStream.write(buff, 0, count);
+        }
 
     }
 }
