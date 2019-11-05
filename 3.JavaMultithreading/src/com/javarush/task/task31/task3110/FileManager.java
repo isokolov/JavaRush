b2653c57353293017544fe23fd94936917b54a2c
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-05 22:33:17 +0200

    Archiver(9)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+    private Path rootPath;
+    private List<Path> fileList;
+
+    public FileManager(Path rootPath) throws IOException {
+        this.rootPath = rootPath;
+        fileList = new ArrayList<>();
+        collectFileList(rootPath);
+    }
+
+    public List<Path> getFileList() {
+        return fileList;
+    }
+
+    private void collectFileList(Path path) throws IOException {
+        if (Files.isRegularFile(path)) {
+            fileList.add(rootPath.relativize(path));
+        }
+        if (Files.isDirectory(path)) {
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+                for (Path file : stream) {
+                    collectFileList(file);
+                }
+            }
+        }
+    }
+}
