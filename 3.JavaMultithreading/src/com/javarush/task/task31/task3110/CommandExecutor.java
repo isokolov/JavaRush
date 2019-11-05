c3cb963cf8913be90d30250db303c57241af143c
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-05 22:19:05 +0200

    Archiver(7)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -2,22 +2,20 @@ package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.*;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CommandExecutor {
-    private static final Map<Operation, Command> allKnownCommandsMap = Stream.of(
-            new ExitCommand(),
-            new ZipAddCommand(),
-            new ZipContentCommand(),
-            new ZipCreateCommand(),
-            new ZipExtractCommand(),
-            new ZipRemoveCommand()
-    ).collect(
-            Collectors.toMap(Command::getOperation, Function.identity())
-    );
+    private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
+
+    static {
+        allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
+        allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
+    }
 
     private CommandExecutor() {
     }
