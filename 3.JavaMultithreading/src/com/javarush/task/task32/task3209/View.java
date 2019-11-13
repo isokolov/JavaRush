15ae3977b0d4c45c0b5736afc1308b4afe4de393
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-13 21:46:31 +0200

    HTML Editor(4)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -19,9 +21,24 @@ public class View extends JFrame implements ActionListener {
         this.controller = controller;
     }
 
-    public void init() {}
+    public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
+
+    }
 
     public void exit() { controller.exit(); }
+
+    public void initMenuBar() {}
+
+    public void initEditor() {}
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
