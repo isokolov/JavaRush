05c2438a05b252be0994026b87d8465c8d412c8f
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-24 21:44:11 +0200

    HTML Editor(10)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -75,4 +75,8 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() {}
+
+    public boolean canUndo(){ return false;}
+
+    public boolean canRedo(){ return false;}
 }
