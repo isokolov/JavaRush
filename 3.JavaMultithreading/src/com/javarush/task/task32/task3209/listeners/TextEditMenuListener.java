f0cc55451409127e79ec3cbf8ea6d2bd7ab403f2
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-24 21:24:11 +0200

    HTML Editor(8)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task32.task3209.listeners;
+
+import com.javarush.task.task32.task3209.View;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class TextEditMenuListener implements MenuListener {
+
+    private View view;
+    private JMenuItem undoMenuItem;
+    private JMenuItem redoMenuItem;
+
+    public TextEditMenuListener(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
