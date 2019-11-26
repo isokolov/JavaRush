661542fcd8f4cce8f7b04e8df0ba727fdc714c24
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-26 14:48:48 +0200

    HTML Editor(11)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -2,18 +2,23 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
-
     private Controller controller;
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
 
     public View() {
         try {
@@ -21,6 +26,10 @@ public class View extends JFrame implements ActionListener {
         } catch (Exception e) {
             ExceptionHandler.log(e);
         }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 
     public Controller getController() {
@@ -31,14 +40,20 @@ public class View extends JFrame implements ActionListener {
         this.controller = controller;
     }
 
-    public void init() {
-        initGui();
-        addWindowListener(new FrameListener(this));
-        setVisible(true);
-
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 
-    public void exit() { controller.exit(); }
+    public void init() {
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        this.addWindowListener(listener);
+        this.setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
 
     public void initMenuBar() {
         JMenuBar menuBar = new JMenuBar();
@@ -54,14 +69,11 @@ public class View extends JFrame implements ActionListener {
 
     public void initEditor() {
         htmlTextPane.setContentType("text/html");
-        JScrollPane jScrollPane = new JScrollPane(htmlTextPane);
-        tabbedPane.add("HTML", jScrollPane);
-        JScrollPane jScrollPane2 = new JScrollPane(plainTextPane);
-        tabbedPane.add("Текст", jScrollPane2);
+        tabbedPane.add("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.add("Текст", new JScrollPane(plainTextPane));
         tabbedPane.setPreferredSize(new Dimension(400, 1000));
         tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
         getContentPane().add(tabbedPane, BorderLayout.CENTER);
-
     }
 
     public void initGui() {
@@ -70,13 +82,37 @@ public class View extends JFrame implements ActionListener {
         pack();
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void selectedTabChanged() {
+
     }
 
-    public void selectedTabChanged() {}
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
 
-    public boolean canUndo(){ return false;}
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
 
-    public boolean canRedo(){ return false;}
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException cue) {
+            ExceptionHandler.log(cue);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException cue) {
+            ExceptionHandler.log(cue);
+        }
+    }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
+    }
+
+
 }
