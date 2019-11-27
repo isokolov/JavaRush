446ac959d329a8e82c053e56c0150cca058c90bc
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-27 22:01:47 +0200

    Aggregator(4)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,4 +1,8 @@
 package com.javarush.task.task28.task2810.model;
 
 public class HHStrategy implements Strategy {
+
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%s";
+
+
 }
