81788961b0efe74cf4e80da339e3f9b8d6ddeb0a
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-27 16:17:52 +0200

    Sokoban(5)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/CollisionObject.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/CollisionObject.java
@@ -8,16 +8,16 @@ public abstract class CollisionObject extends GameObject {
     public boolean isCollision(GameObject gameObject, Direction direction) {
         switch (direction) {
             case LEFT: {
-                return getX() - Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY();
+                return (getX() - Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY()) ? true : false;
             }
             case RIGHT: {
-                return getX() + Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY();
+                return (getX() + Model.FIELD_CELL_SIZE == gameObject.getX() && gameObject.getY() == this.getY()) ? true : false;
             }
             case DOWN: {
-                return gameObject.getX() == this.getX() && getY() + Model.FIELD_CELL_SIZE == gameObject.getY();
+                return (gameObject.getX() == this.getX() && getY() + Model.FIELD_CELL_SIZE == gameObject.getY()) ? true : false;
             }
             case UP: {
-                return gameObject.getX() == this.getX() && getY() - Model.FIELD_CELL_SIZE == gameObject.getY();
+                return (gameObject.getX() == this.getX() && getY() - Model.FIELD_CELL_SIZE == gameObject.getY()) ? true : false;
             }
             default:
                 return false;
