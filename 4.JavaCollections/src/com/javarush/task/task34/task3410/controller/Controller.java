4bec024f6be0d1347ed1c9eb16522b952b9519c0
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-27 20:51:13 +0200

    Sokoban(13)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/controller/Controller.java
@@ -14,7 +14,9 @@ public class Controller implements EventListener {
         view = new View(this);
         model = new Model();
         view.init();
+        view.setEventListener(this);
         model.restart();
+        model.setEventListener(this);
     }
 
 
@@ -28,21 +30,24 @@ public class Controller implements EventListener {
 
     @Override
     public void move(Direction direction) {
-
+        model.move(direction);
+        view.update();
     }
 
     @Override
     public void restart() {
-
+        model.restart();
+        view.update();
     }
 
     @Override
     public void startNextLevel() {
-
+        model.startNextLevel();
+        view.update();
     }
 
     @Override
     public void levelCompleted(int level) {
-
+        view.completed(level);
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -32,4 +32,8 @@ public class Model {
         restartLevel(currentLevel);
     }
 
+    public void move (Direction direction) {
+
+    }
+
 }
