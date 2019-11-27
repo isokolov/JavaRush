3f2cc526729cd8ea9c924b1ac6662de3f51d0ee9
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-27 20:46:58 +0200

    Sokoban(12)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -2,9 +2,11 @@ package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.Controller;
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 public class Field extends JPanel {
 
@@ -18,7 +20,13 @@ public class Field extends JPanel {
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 300, 300);
+        Set<GameObject> gameObjectSet = view.getGameObjects().getAll();
+
+        for (GameObject gameObject : gameObjectSet) {
+            gameObject.draw(g);
+        }
     }
 
     public void setEventListener(EventListener eventListener) {
