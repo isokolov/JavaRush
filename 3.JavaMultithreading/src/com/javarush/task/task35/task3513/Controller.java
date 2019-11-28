dbe169158270dcc8cf3c789d8213d27fd9bc5750
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-28 16:04:20 +0200

    2048(9)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,14 +1,16 @@
 package com.javarush.task.task35.task3513;
 
 import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Controller extends KeyAdapter {
-
+    private final static int WINNING_TILE = 2048;
     private Model model;
-
+    private View view;
 
     public Controller(Model model) {
         this.model = model;
+        this.view = new View(this);
     }
 
     public Tile[][] getGameTiles() {
@@ -17,5 +19,44 @@ public class Controller extends KeyAdapter {
 
     public int getScore() {
         return model.score;
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        model.maxTile = 0;
+        view.isGameWon = false;
+        view.isGameLost = false;
+        model.resetGameTiles();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        } else if (!model.canMove()) {
+            view.isGameLost = true;
+        } else if (!view.isGameLost && !view.isGameWon) {
+            processStep(e.getKeyCode());
+            if (model.maxTile == WINNING_TILE) {
+                view.isGameWon = true;
+            }
+        }
+        view.repaint();
+    }
+
+    private void processStep(int keyCode) {
+        if (keyCode == KeyEvent.VK_LEFT) {
+            model.left();
+        } else if (keyCode == KeyEvent.VK_RIGHT) {
+            model.right();
+        } else if (keyCode == KeyEvent.VK_UP) {
+            model.up();
+        } else if (keyCode == KeyEvent.VK_DOWN) {
+            model.down();
+        }
+    }
+
+    public View getView() {
+        return view;
     }
 }
