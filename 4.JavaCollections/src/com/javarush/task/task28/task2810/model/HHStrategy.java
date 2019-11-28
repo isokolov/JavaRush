4b9c6e7fac89bef02ae86387271682e339da05d7
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-28 17:06:38 +0200

    Aggregator(6)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,17 +1,33 @@
 package com.javarush.task.task28.task2810.model;
 
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**  Парсит http://hh.ru
+ https://novosibirsk.hh.ru/search/vacancy?text=java+Новосибирск
+ https://hh.ru/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE */
 public class HHStrategy implements Strategy {
-
-    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%s";
+    private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
+    private static final String userAgent = "Mozilla/5.0 (jsoup)";
+    private static final int timeout = 5 * 1000;
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        ArrayList<Vacancy> res = new ArrayList<>();
+        ArrayList<Vacancy> res = new ArrayList<Vacancy>();
+        Document doc;
+        try {
+            doc = Jsoup.connect(String.format(URL_FORMAT, searchString, 0)).get();
+
+            //System.out.println(doc.html());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return res;
     }
 }
