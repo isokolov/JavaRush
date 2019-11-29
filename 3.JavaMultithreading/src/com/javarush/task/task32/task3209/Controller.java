f095167580c112d29903cb3cd7f88ecb908bce2b
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-29 09:57:42 +0200

    HTML Editor(22)

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,15 +1,14 @@
 package com.javarush.task.task32.task3209;
 
-import javax.swing.text.BadLocationException;
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 
 public class Controller {
-
     private View view;
     private HTMLDocument document;
     private File currentFile;
@@ -18,17 +17,45 @@ public class Controller {
         this.view = view;
     }
 
+    public static void main(String[] args) {
+        View view = new View();
+        Controller controller = new Controller(view);
+        view.setController(controller);
+        view.init();
+        controller.init();
+    }
+
+    public void init() {
+        createNewDocument();
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
     public HTMLDocument getDocument() {
         return document;
     }
 
     public void resetDocument() {
-        if (document != null) {
+        if (document != null && view != null) {
             document.removeUndoableEditListener(view.getUndoListener());
         }
         document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
-        document.addUndoableEditListener(view.getUndoListener());
+        if (document.getUndoableEditListeners() == null) {
+            document.addUndoableEditListener(view.getUndoListener());
+        }
         view.update();
+    }
+
+    public String getPlainText() {
+        StringWriter writer = new StringWriter();
+        try {
+            new HTMLEditorKit().write(writer, document, 0, document.getLength());
+        } catch (Exception e) {
+            ExceptionHandler.log(e);
+        }
+        return writer.toString();
     }
 
     public void setPlainText(String text) {
@@ -36,26 +63,16 @@ public class Controller {
         StringReader reader = new StringReader(text);
         try {
             new HTMLEditorKit().read(reader, document, 0);
-        } catch (IOException | BadLocationException e) {
+        } catch (Exception e) {
             ExceptionHandler.log(e);
         }
-    }
-
-    public String getPlainText() {
-        StringWriter writer = new StringWriter();
-        try {
-            new HTMLEditorKit().write(writer, document, 0, document.getLength());
-        } catch (IOException|BadLocationException e) {
-            ExceptionHandler.log(e);
-        }
-        return writer.toString();
     }
 
     public void createNewDocument() {
         view.selectHtmlTab();
-        resetDocument();
-        view.setTitle("HTML редактор");
         view.resetUndo();
+        resetDocument();
+        view.setTitle("HTML editor");
         currentFile = null;
     }
 
@@ -68,20 +85,21 @@ public class Controller {
     }
 
     public void saveDocumentAs() {
+        view.selectHtmlTab();
 
-    }
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new HTMLFileFilter());
 
-    public void init() {
-        createNewDocument();
-    }
+        if (fileChooser.showSaveDialog(view) == JFileChooser.APPROVE_OPTION) {
+            currentFile = fileChooser.getSelectedFile();
+            view.setTitle(currentFile.getName());
 
-    public void exit() { System.exit(0);}
+            try (FileWriter writer = new FileWriter(currentFile)) {
+                new HTMLEditorKit().write(writer, document, 0, document.getLength());
+            } catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
 
-    public static void main(String[] args) {
-        View view = new View();
-        Controller controller = new Controller(view);
-        view.setController(controller);
-        view.init();
-        controller.init();
+        }
     }
 }
