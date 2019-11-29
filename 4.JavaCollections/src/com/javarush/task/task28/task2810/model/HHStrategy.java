df5c3eef5155e7d6f3d354d1bf866507aabfdd52
Illya Sokolov <illya.sokolov82@gmail.com>
2019-11-29 08:54:06 +0200

    Aggregator(7)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -13,15 +13,20 @@ import java.util.List;
  https://hh.ru/search/vacancy?text=java+ADDITIONAL_VALUE&page=PAGE_VALUE */
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ru/search/vacancy?text=java+%s&page=%d";
-    private static final String userAgent = "Mozilla/5.0 (jsoup)";
+    private static final String userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
+    private static final String referrer = "referrer";
     private static final int timeout = 5 * 1000;
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
-        ArrayList<Vacancy> res = new ArrayList<Vacancy>();
+        ArrayList<Vacancy> res = new ArrayList<>();
         Document doc;
+
         try {
-            doc = Jsoup.connect(String.format(URL_FORMAT, searchString, 0)).get();
+            doc = Jsoup.connect(String.format(URL_FORMAT, searchString, 0))
+                    .userAgent(userAgent)
+                    .referrer(referrer)
+                    .get();
 
             //System.out.println(doc.html());
         } catch (IOException e) {
