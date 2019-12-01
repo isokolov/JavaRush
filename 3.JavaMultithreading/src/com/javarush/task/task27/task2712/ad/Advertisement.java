2899b979c7db053d8c1166533d6bed31a59f46b7
Illya Sokolov <illya.sokolov82@gmail.com>
2019-12-01 15:06:11 +0200

    Restaurant(7)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -1,13 +1,12 @@
 package com.javarush.task.task27.task2712.ad;
 
 public class Advertisement {
+    private Object content;
+    private String name;
+    private long initialAmount;
+    private int hits;
+    private int duration;
 
-    private Object content; // видео
-    private String name; // имя/название
-    private long initialAmount; // начальная сумма, стоимость рекламы в копейках. Используем long, чтобы избежать проблем с округлением
-    private int hits; // количество оплаченных показов
-    private int duration; // продолжительность в секундах
-    private long amountPerOneDisplaying;
 
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
@@ -15,7 +14,28 @@ public class Advertisement {
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
-        amountPerOneDisplaying = initialAmount / hits;
+
     }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getInitialAmount() {
+        return initialAmount;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
 
 }
