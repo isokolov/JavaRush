973dee61cb10da8f29cfef0ab1edcb53bd9246ca
Illya Sokolov <illya.sokolov82@gmail.com>
2019-12-01 14:25:59 +0200

    Restaurant(3)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,10 +3,11 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable {
     protected final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -18,22 +19,23 @@ public class Tablet {
         return number;
     }
 
-    public void createOrder()
-    {
+    public Order createOrder() {
         try
         {
             Order order = new Order(this);
+            if(order.isEmpty())
+                return null;
 
-            if (!order.isEmpty())
-            {
-                ConsoleHelper.writeMessage(order.toString());
-            }
+            ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
+            return order;
         }
         catch (IOException e)
         {
             logger.log(Level.SEVERE, "Console is unavailable.");
         }
-
+        return null;
     }
 
     @Override
---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Cook.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task27.task2712.kitchen;
+
+
+import com.javarush.task.task27.task2712.ConsoleHelper;
+
+import java.util.Observable;
+import java.util.Observer;
+import java.util.logging.Logger;
+
+public class Cook extends Observable implements Observer
+{
+    private String name;
+    private static Logger logger = Logger.getLogger(Cook.class.getName());
+
+    public Cook(String name)
+    {
+        this.name = name;
+    }
+
+    public void update(Observable observable, Object arg)
+    {
+        ConsoleHelper.writeMessage(String.format("Start cooking - %s", arg));
+        setChanged();
+        notifyObservers(arg);
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+}
