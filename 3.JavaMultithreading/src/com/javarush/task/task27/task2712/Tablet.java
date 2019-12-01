d787d1efe236646375c54ea01d0cbbc9b47ad773
Illya Sokolov <illya.sokolov82@gmail.com>
2019-12-01 14:21:52 +0200

    Restaurant(2)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -1,12 +1,45 @@
 package com.javarush.task.task27.task2712;
 
-public class Tablet {
+import com.javarush.task.task27.task2712.kitchen.Order;
 
-    private final int number;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Tablet {
+    protected final int number;
+    private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    public void createOrder() {}
+    public int getNumber() {
+        return number;
+    }
+
+    public void createOrder()
+    {
+        try
+        {
+            Order order = new Order(this);
+
+            if (!order.isEmpty())
+            {
+                ConsoleHelper.writeMessage(order.toString());
+            }
+        }
+        catch (IOException e)
+        {
+            logger.log(Level.SEVERE, "Console is unavailable.");
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "Tablet{" +
+                "number=" + number +
+                '}';
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -1,16 +1,50 @@
 package com.javarush.task.task27.task2712.kitchen;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
 import com.javarush.task.task27.task2712.Tablet;
 
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.List;
 
 public class Order {
+    protected List<Dish> dishes;
+    private Tablet tablet;
 
-    private final Tablet tablet;
-    protected List<Dish> dishes = new ArrayList<>();
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        initDishes();
     }
+
+    protected void initDishes() throws IOException {
+        ConsoleHelper.writeMessage(Dish.allDishesToString());
+        dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        if (dishes.isEmpty()) return "";
+        return "Your order: " + dishes + " of " + tablet;
+    }
+
+    public int getTotalCookingTime() {
+        int sum = 0;
+        for (Dish d : dishes) {
+            sum += d.getDuration();
+        }
+        return sum;
+    }
+
+    public boolean isEmpty() {
+        return dishes.isEmpty();
+    }
+
+    public Tablet getTablet() {
+        return tablet;
+    }
+
+    public List<Dish> getDishes() {
+        return dishes;
+    }
+
 }
