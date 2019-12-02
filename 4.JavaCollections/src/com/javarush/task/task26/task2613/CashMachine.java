e49dadc847dad4a9b13aab903617c6a221ec4be0
Illya Sokolov <illya.sokolov82@gmail.com>
2019-12-02 10:00:33 +0200

    CashMashine(15)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/CashMachine.java
@@ -6,18 +6,19 @@ import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 import java.util.Locale;
 
 public class CashMachine {
+    public static final String RESOURCE_PATH = CashMachine.class.getPackage().getName() + ".resources.";
+
     public static void main(String[] args) {
         Locale.setDefault(Locale.ENGLISH);
         try {
             Operation o = Operation.LOGIN;
             CommandExecutor.execute(o);
-
             do {
                 o = ConsoleHelper.askOperation();
                 CommandExecutor.execute(o);
             }while (o != null && !o.equals(Operation.EXIT));
         } catch (InterruptOperationException e) {
-            ConsoleHelper.writeMessage("By By, my love!");
+            ConsoleHelper.printExitMessage();
         }
     }
 }
